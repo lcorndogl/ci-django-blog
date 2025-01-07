9db07b4bd9dc13a34790a05171f21ca202115440
Michael Cornall <mik.reds.forever@gmail.com>
2025-01-07 20:35:23 +0000

    Add POST processing to collab form

--- about/views.py
+++ about/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 # Create your views here.
@@ -10,6 +12,18 @@ def about_me(request):
     """
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
+
+
+    if request.method == "POST":
+        print("Received a POST request")
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            comment = collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working days'
+    )
+            collaborate_form = CollaborateForm()
 
 
     return render(
