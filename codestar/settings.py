77c3aaeca7d96a3c33ebc5e632b87c2740488262
Michael Cornall <mik.reds.forever@gmail.com>
2025-01-07 12:50:40 +0000

    I forgot

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEBUG' in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com', '.codeinstitute-ide.net']
 
 
 # Application definition
